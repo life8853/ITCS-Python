477e67a971dbfd7f2cbab07782376ed9d791880e
Jakub Ochal <life88853@gmail.com>
2023-11-12 11:18:28 +0100

    Fibonacci numbers calculator - Lab Assigment 4

---
+++ module_with_documentation_fibonacci/main.py
@@ -0,0 +1,11 @@
+"""
+This is the main program file
+"""
+import fib
+
+if __name__ == "__main__":
+    print("Using functions from fib library:")
+    print(fib.fib(50))
+    print(fib.fib_r(35))
+    print(fib.fib_series(10))
+    print(fib.fib_upto(60))
